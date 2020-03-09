f51e53220489ec42bf3774d391df291e40229e81
David M Kinnen <davidkinnen@inspirationtrust.org>
2020-03-09 20:05:35 +0000

    Modified program so it now calculates area and perimeter for a rectangle.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,58 @@ namespace Buckhaven_5_Shapes_CSharp
     {
         static void Main(string[] args)
         {
+            Rectangle rect1 = new Rectangle();
+
+            Console.WriteLine($"What is the length of rectangle 1?");
+            rect1.RectLength = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"What is the width of rectangle 1?");
+            rect1.RectWidth = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+            rect1.CalculateArea();
+            rect1.CalculatePerimeter();
+
+            Console.WriteLine($"The area of a rectangle width {rect1.RectWidth} and length {rect1.RectLength} is {rect1.RectArea}.");
+            Console.WriteLine($"The perimeter is {rect1.RectPerimeter}.");
+            Console.ReadLine();
+        }
+
+        public class Rectangle
+        {
+            private int _rectWidth;
+            private int _rectLength;
+            private int _rectArea;
+            private int _rectPerimeter;
+
+            public int RectWidth
+            {
+                get { return _rectWidth; }
+                set { _rectWidth = value; }
+            }
+            public int RectLength
+            {
+                get { return _rectLength; }
+                set { _rectLength = value; }
+            }
+
+            public int RectArea
+            {
+                get { return _rectArea; }
+            }
+
+            public int RectPerimeter
+            {
+                get { return _rectPerimeter; }
+            }
+
+            public void CalculateArea()
+            {
+                _rectArea = _rectWidth* _rectLength;
+            }
+            public void CalculatePerimeter()
+            {
+                _rectPerimeter = (2 * _rectWidth) + (2 * _rectLength);
+            }
+
         }
     }
 }
